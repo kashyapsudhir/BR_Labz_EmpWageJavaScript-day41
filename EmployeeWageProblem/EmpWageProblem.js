a496cac07b44dfd2d9477fac4c1faec5925c624d
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-16 14:17:34 +0530

    [check employee wage basis on function]

--- EmployeeWageProblem/EmpWageProblem.js
+++ EmployeeWageProblem/EmpWageProblem.js
@@ -15,8 +15,36 @@ else
     console.log("Employee is Present")
 
 }
-*/
+
 //UC-2
+let IS_FULL_TIME=1;
+let IS_PART_TIME=2;
+let EMP_RATE_PER_HR=20;
+let empHrs=0;
+let empWage=0;
+let empInput=Math.floor((Math.random()*10)%3);
+
+switch(empInput)
+{
+    case IS_FULL_TIME:
+        console.log("Employee present full time");
+        empHrs=8;
+        break;
+    case IS_PART_TIME:
+        console.log("Employee present part time");
+        empHrs=4;
+        break;
+    default:
+        console.log("Employee is absent");
+        empHrs=0;
+}
+empWage=EMP_RATE_PER_HR*empHrs;
+console.log("Employee wage is:",empWage);
+
+*/
+
+//UC-3
+
 let IS_FULL_TIME=1;
 let IS_PART_TIME=2;
 let EMP_RATE_PER_HR=20;
