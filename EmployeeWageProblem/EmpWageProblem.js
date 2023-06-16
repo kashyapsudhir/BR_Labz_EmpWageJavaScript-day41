87114b2b567e626e9e41dfdc8d4c1f5433211d50
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-16 14:36:28 +0530

    [store in array]

--- EmployeeWageProblem/EmpWageProblem.js
+++ EmployeeWageProblem/EmpWageProblem.js
@@ -105,7 +105,7 @@ for(var day=0;day<=MAX_WORKING_DAYS;day++)
 }
 totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
 console.log("Total Employee wage is:",totalEmpWage);
-*/
+
 //UC-5
 
 let IS_FULL_TIME=1;
@@ -142,5 +142,51 @@ while(day<=MAX_WORKING_DAYS && totalEmpHrs<=MAX_WORKING_HRS)
     totalEmpHrs+=empHrs;
     day++;
 }
+totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
+console.log("Total Employee wage for "+(day-1)+" days or "+(totalEmpHrs)+" working Hrs is:"+totalEmpWage);
+*/
+//UC-6
+let IS_FULL_TIME=1;
+let IS_PART_TIME=2;
+let EMP_RATE_PER_HR=20;
+const MAX_WORKING_DAYS=20;
+const MAX_WORKING_HRS=160;
+let day=0;
+let empHrs=0,totalEmpHrs=0;
+let empWage=0,totalEmpWage=0;
+
+let empDailyWageArray=new Array();
+
+
+function GetEmpHours(empInput)
+{ 
+    switch(empInput)
+    {
+        case IS_FULL_TIME:
+            
+            return empHrs=8;
+            
+        case IS_PART_TIME:
+            
+            return empHrs=4;
+            
+        default:
+            
+            return empHrs=0;
+    }
+}
+while(day<=MAX_WORKING_DAYS && totalEmpHrs<=MAX_WORKING_HRS)
+{
+    let empInput=Math.floor((Math.random()*10)%3);
+    empWage=EMP_RATE_PER_HR*GetEmpHours(empInput);
+    totalEmpHrs+=empHrs;
+    empDailyWageArray.push(computeEmpDailyWage(empHrs));
+    day++;
+}
+function computeEmpDailyWage(empHrs)
+{
+    return empHrs*EMP_RATE_PER_HR;
+}
+console.log("employee daily wage is :\n"+empDailyWageArray);
 totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
 console.log("Total Employee wage for "+(day-1)+" days or "+(totalEmpHrs)+" working Hrs is:"+totalEmpWage);
