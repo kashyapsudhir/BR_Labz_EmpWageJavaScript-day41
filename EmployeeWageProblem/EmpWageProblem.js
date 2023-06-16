dcdc41574c552389712d96d6dd1a0e1074a86bed
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-16 14:06:28 +0530

    [check employee is absent or present by using random function]

--- EmployeeWageProblem/EmpWageProblem.js
+++ EmployeeWageProblem/EmpWageProblem.js
@@ -1,1 +1,17 @@
 console.log("Welcome to employee Wage Problem day41");
+
+//UC-1
+const Is_Absent = 0;
+let employeeCheck = Math.floor(Math.random()*10)%2;
+if(employeeCheck == Is_Absent)
+{
+    console.log("Employee is Absent");
+    return;
+
+
+}
+else
+{
+    console.log("Employee is Present")
+
+}
